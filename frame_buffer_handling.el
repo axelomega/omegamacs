1a9e3aa9d8ce2a5758537a0076407385729b13f3
Per Karlström <dev@per.karlstrom.se>
2025-08-22 22:14:03 -0400

    More cleanup changes

--- frame_buffer_handling.el
+++ frame_buffer_handling.el
@@ -21,17 +21,8 @@
   (sml-modeline-mode 1) ;; show buffer pos in the mode line
   (scroll-bar-mode -1)) ;; turn off the scrollbar
 
-;;Easy frame switching
-;;;(global-set-key (kbd "C-c o") 'other-frame)
-;;;(require 'framemove)
-;;;(windmove-default-keybindings)
-;;;(setq framemove-hook-into-windmove t)
-
 ;;Show paranthesis
 (setq show-paren-delay 0)           ; how long to wait?
 (show-paren-mode t)                 ; turn paren-mode on
 (setq show-paren-style 'expression) ; alternatives are 'parenthesis' and 'mixed'
-;;;(set-face-background 'show-paren-match-face "#000033")
-;;;(set-face-attribute 'show-paren-match-face nil
-;;;        :weight 'bold :underline nil :overline nil :slant 'normal)
 
