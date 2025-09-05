0a2cf43c9778729aa2879032d2e476787bcdc37b
Per Karlström <dev@per.karlstrom.se>
2025-09-05 01:10:24 -0400

    load the mode

---
+++ languages/yaml.el
@@ -0,0 +1,10 @@
+;;; -*- lexical-binding: t -*-
+
+;; YAML mode for editing YAML files
+(use-package yaml-mode
+  :ensure t
+  :mode ("\\.ya?ml\\'" . yaml-mode)
+  :config
+  (add-hook 'yaml-mode-hook
+            (lambda ()
+              (define-key yaml-mode-map "\C-m" 'newline-and-indent))))
