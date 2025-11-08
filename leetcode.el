6516e0a68f4bd6a8cb6ccd63cdd07ec9c3da43cc
Per Karlström <dev@per.karlstrom.se>
2025-11-08 00:40:27 -0500

    add leetcode package

---
+++ leetcode.el
@@ -0,0 +1,24 @@
+;;; -*- lexical-binding: t -*-
+
+;; LeetCode client for Emacs
+(use-package leetcode
+  :ensure t
+  :defer omegamacs-enable-lazy-loading
+  :config
+  ;; Set preferred programming language
+  (setq leetcode-prefer-language "cpp")
+
+  ;; Set preferred SQL dialect
+  (setq leetcode-prefer-sql "mysql")
+
+  ;; Directory for saving solutions
+  (setq leetcode-directory "~/leetcode")
+
+  ;; Enable automatic solution saving
+  (setq leetcode-save-solutions t)
+
+  ;; Toggle tag display in problem list
+  (setq leetcode-prefer-tag-display t))
+
+;; Keybindings for LeetCode
+(global-set-key (kbd "C-c l") 'leetcode)
